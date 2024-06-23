adadbf1ec39960a35cd6e64314c260727e2aef7e
jithu-soe <97289934+jithu-soe@users.noreply.github.com>
2024-06-23 07:32:15 +0000

    first home route created

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("jithu");
 // explicitly commit to push your code to github
+const app = require('./app')
+const {PORT}= process.env
+app.listen(PORT,()=>{
+    console.log(`SERVER running at port ${PORT}`);
+})
